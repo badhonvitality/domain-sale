63c68b6a38f0ba458ae0908fd97903fe61c83fca
Badhon Vitality <info.nvidiacore@gmail.com>
2025-08-04 00:00:24 +0600

    Update fix layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -524,38 +524,38 @@ export default function RootLayout({
         <link rel="manifest" href="/manifest.json" />
 
 
-      {/* ✅ Hotjar Analytics */}
+       {/* ✅ Hotjar Analytics */}
       <Script id="hotjar" strategy="afterInteractive">
         {`
           (function(h,o,t,j,a,r){
-            h.hj = h.hj || function() {(h.hj.q = h.hj.q || []).push(arguments)};
-            h._hjSettings = { hjid: 6482824, hjsv: 6 };
-            a = o.getElementsByTagName('head')[0];
-            r = o.createElement('script'); r.async = 1;
-            r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:6482824,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
             a.appendChild(r);
-          })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
         `}
       </Script>
 
       {/* ✅ Ahrefs Analytics */}
-        <script src="https://analytics.ahrefs.com/analytics.js" data-key="VPxbncXMeSzwDXKvNR3MVw" async></script>
+      <Script
+        src="https://analytics.ahrefs.com/analytics.js"
+        data-key="VPxbncXMeSzwDXKvNR3MVw"
+        strategy="afterInteractive"
+        async
+      />
 
       {/* ✅ Microsoft Clarity Analytics */}
-      <Script
-        id="clarity"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            (function(c,l,a,r,i,t,y){
-              c[a] = c[a] || function() { (c[a].q = c[a].q || []).push(arguments) };
-              t = l.createElement(r); t.async = 1;
-              t.src = "https://www.clarity.ms/tag/" + i;
-              y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "sp5vev7n6t");
-          `,
-        }}
-      />
+      <Script id="microsoft-clarity" strategy="afterInteractive">
+        {`
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/" + i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "sp5vev7n6t");
+        `}
+      </Script>
 
 
         {/* ✅ Google Analytics */}
