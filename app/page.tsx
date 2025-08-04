24cdb76a1e6a837dc74a828988cfe82ac1f55b49
Badhon Vitality <info.nvidiacore@gmail.com>
2025-08-04 14:40:58 +0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -109,14 +109,6 @@ export default function HomePage() {
         strategy="afterInteractive"
         crossOrigin="anonymous"
       />
-        <script
-    nowprocket=""
-    nitro-exclude=""
-    type="text/javascript"
-    id="sa-dynamic-optimization"
-    data-uuid="4fbde82c-2f38-46f2-bb77-8efc36415e0d"
-    src="data:text/javascript;base64,dmFyIHNjcmlwdCA9IGRvY3VtZW50LmNyZWF0ZUVsZW1lbnQoInNjcmlwdCIpO3NjcmlwdC5zZXRBdHRyaWJ1dGUoIm5vd3Byb2NrZXQiLCAiIik7c2NyaXB0LnNldEF0dHJpYnV0ZSgibml0cm8tZXhjbHVkZSIsICIiKTtzY3JpcHQuc3JjID0gImh0dHBzOi8vZGFzaGJvYXJkLnNlYXJjaGF0bGFzLmNvbS9zY3JpcHRzL2R5bmFtaWNfb3B0aW1pemF0aW9uLmpzIjtzY3JpcHQuZGF0YXNldC51dWlkID0gIjRmYmRlODJjLTJmMzgtNDZmMi1iYjc3LThlZmMzNjQxNWUwZCI7c2NyaXB0LmlkID0gInNhLWR5bmFtaWMtb3B0aW1pemF0aW9uLWxvYWRlciI7ZG9jdW1lbnQuaGVhZC5hcHBlbmRDaGlsZChzY3JpcHQpOw=="
-  ></script>
       
     </>
   )
