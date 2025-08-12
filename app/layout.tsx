fed6814b0cfe3fa1109ec13c48395e35b9881889
Badhon Vitality <info.nvidiacore@gmail.com>
2025-08-12 09:52:51 +0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -868,77 +868,8 @@ export default function RootLayout({
         />
       </head>
 <body className={`${inter.className} antialiased`}>
-  <div style={{ position: "absolute", zIndex: 99999 }}>
-    <input autoComplete="off" type="checkbox" id="aadsstickyme7zy2vu" hidden />
-    <div style={{ paddingTop: 0, paddingBottom: "auto" }}>
-      <div
-        style={{
-          width: "100%",
-          height: "auto",
-          position: "fixed",
-          textAlign: "center",
-          fontSize: 0,
-          bottom: 0,
-          left: 0,
-          right: 0,
-          margin: "auto",
-        }}
-      >
-        <label
-          htmlFor="aadsstickyme7zy2vu"
-          style={{
-            top: "50%",
-            transform: "translateY(-50%)",
-            right: "24px",
-            position: "absolute",
-            borderRadius: 4,
-            background: "rgba(248, 248, 249, 0.70)",
-            padding: 4,
-            zIndex: 99999,
-            cursor: "pointer",
-          }}
-        >
-          <svg
-            fill="#000000"
-            height="16px"
-            width="16px"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 490 490"
-          >
-            <polygon points="456.851,0 245,212.564 33.149,0 0.708,32.337 212.669,245.004 0.708,457.678 33.149,490 245,277.443 456.851,490 489.292,457.678 277.331,245.004 489.292,32.337 " />
-          </svg>
-        </label>
-        <div
-          id="frame"
-          style={{
-            width: "100%",
-            margin: "auto",
-            background: "rgba(0, 0, 0, 0.50)",
-            position: "relative",
-            zIndex: 99998,
-          }}
-        >
-          <iframe
-            data-aa="2406432"
-            src="//acceptable.a-ads.com/2406432/?size=Adaptive"
-            style={{
-              border: 0,
-              padding: 0,
-              width: "70%",
-              height: "auto",
-              overflow: "hidden",
-              margin: "auto",
-            }}
-          ></iframe>
-        </div>
-      </div>
-      <style>{`
-        #aadsstickyme7zy2vu:checked + div {
-          display: none;
-        }
-      `}</style>
-    </div>
-  </div>
+
+  <div id="frame" style="width: 100%;"><iframe data-aa='2406432' src='//acceptable.a-ads.com/2406432' style='border:0px; padding:0; width:100%; height:100%; overflow:hidden; background-color: transparent;'></iframe><a style="display: block; text-align: right; font-size: 12px" id="frame-link" href="https://aads.com/campaigns/new/?source_id=2406432&source_type=ad_unit&partner=2406432">Advertise here</a></div>
   <Suspense
     fallback={
       <div className="min-h-screen bg-black flex items-center justify-center">
